192b9823ff6783c7a25c16ae0d0ac082c9b57193
zippien <arulblood@gmail.com>
2017-08-13 17:59:45 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -26,7 +26,7 @@ if(x=="1"){xxx="/2017/08/percayalah-kamu-hanya-memilih-1-dari-25.html"}
 if(x=="2"){xxx="/2017/08/percayalah-kamu-hanya-memilih-1-dari-25.html"}
 if(x=="3"){xxx="/2017/08/percayalah-kamu-hanya-memilih-1-dari-25.html"}
 if(x=="4"){xxx="/2017/08/percayalah-kamu-hanya-memilih-1-dari-25.html"}
-if(x=="5"){xxx="/2017/08/percayalah-kamu-hanya-memilih-1-dari-25.html"}a_to_ve[i].href="https://zippienlink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/08/percayalah-kamu-hanya-memilih-1-dari-25.html"}a_to_ve[i].href="https://zippien.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
